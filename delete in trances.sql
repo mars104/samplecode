e72298e9eef3afecaa521b2d32dd1642ed2abeab
mars104 <130562471+mars104@users.noreply.github.com>
2023-05-22 14:48:59 +0100

    Create delete in trances.sql

---
+++ delete in trances.sql
@@ -0,0 +1,14 @@
+DECLARE @BatchSize INT = 1000; -- Adjust the batch size as per your requirement
+
+WHILE 1 = 1
+BEGIN
+    DELETE T
+    FROM (
+        SELECT TOP (@BatchSize) ROW_NUMBER() OVER (PARTITION BY BINARY_CHECKSUM(*) ORDER BY %%physloc%%) AS RowNumber, *
+        FROM MyTable
+    ) T
+    WHERE T.RowNumber > 1;
+
+    IF @@ROWCOUNT < @BatchSize
+        BREAK;
+END
